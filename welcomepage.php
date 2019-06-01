eb3044eecf1ef742cf66cc28863cce3312d20d66
S Surya Narayanan <suryan0800@gmail.com>
2019-06-01 15:03:10 +0530

    logout function added to welcomepage

--- welcomepage.php
+++ welcomepage.php
@@ -1,25 +1,23 @@
   <?php
-         session_start();
-         if(empty($_SESSION["user"]))
-         {
-            session_unset();
-            session_destroy();
-            header("Location: login.php");
-            exit();
-         }
-        elseif($_SESSION["date"] != date("d-m-Y"))
-        {           
+        function logout()
+        {
             session_unset();
             session_destroy();
             header("Location: login.php");
             exit();
         }
+         session_start();
+         if(empty($_SESSION["user"]))
+         {
+            logout();
+         }
+        elseif($_SESSION["date"] != date("d-m-Y"))
+        {           
+            logout();
+        }
         elseif(strtotime('+10 minutes',strtotime($_SESSION["time"])) < strtotime(date("h:i:s A")))
         {             
-            session_unset();
-            session_destroy();
-            header("Location: login.php");
-            exit();
+           logout();
         }
         
         ?>
